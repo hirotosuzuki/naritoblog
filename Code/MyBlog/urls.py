06b6030242128c31188e51b9f4cf6683ce39798f
“hirotosuzuki” <hiroto7665@gmail.com>
2018-02-04 09:20:38 +0000

    add urls.py views.py templates

--- Code/MyBlog/urls.py
+++ Code/MyBlog/urls.py
@@ -19,4 +19,5 @@ from django.conf.urls import include, url
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
+    url(r'', include('myblog.urls')),
 ]
